87b6a4c96df026973d5d40df7e7ff9f61dc3d870
shivamtripathi8906 <shivamtripathi159@gmail.com>
2024-01-12 18:55:14 +0530

    First Class

---
+++ console.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+console.log(2+1);
+let a = 6;
+console.log(`${a}`+1+1);
+
+// shreya = 1;
+// console.log(b+1);
+// var b = shreya;
+// console.log(b.typeof);
+/*
+* Console par log karo hello world. Dir means folder. 
+* Hoisting means baad ka declare pehle declare ho jaygea but value baad me dene se 
+* print nhi hoga NaN dekhayega JS runs from top to bottom.
+*/
+
+const x = 100;
+console.log(x);
+
+// const value cannot be changed throughout the program
---
+++ scope.js
@@ -0,0 +1,8 @@
+var name = "shreya";
+
+{
+  let name = "bhavya";
+  console.log(name + "line5");
+}
+
+console.log(name + "line8");
